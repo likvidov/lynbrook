5ad511ade5236fb86f56b541a214817b5800cee5
likvidov <likvidya@gmail.com>
2020-01-29 14:46:16 +0300

    Add all files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+jQuery(function($){
+    $('.homepage_slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        centerPadding: '0px',
+        centerMode: true,
+        draggable:true
+      });
+});
